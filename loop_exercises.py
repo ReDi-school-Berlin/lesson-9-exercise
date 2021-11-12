f2031e44381e1e468f011e1b0c151072bb494eb4
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2021-11-12 16:14:50 +0100

    Update loop_exercises.py

--- loop_exercises.py
+++ loop_exercises.py
@@ -1,2 +1,193 @@
 # Exercise 1:
-# W
+# Write a for loop so that every item in the list is printed
+animals = ["koala", "cat", "fox", "panda", "chipmunk", "sloth", "penguin", "dolphin"]
+
+# --- WRITE YOUR CODE BELOW ---
+
+
+
+
+
+
+
+# Exercise 2:
+# Write a for loop which prints "Hello!, " plus each name in the list. i.e.: "Hello!, Sam"
+names = ["Sam", "Lisa", "Micha", "Dave", "Wyatt", "Emma", "Sage"]
+
+# --- WRITE YOUR CODE BELOW ---
+
+
+
+
+
+
+
+# Exercise 3:
+# Write a for loop which appends the square of each number to the new list.
+numbers = [3, 7, 6, 8, 9, 11, 15, 25]
+
+# --- WRITE YOUR CODE BELOW ---
+
+
+
+
+
+
+
+# Exercise 4:
+# Write a for loop using an if statement, that appends each number to the new list if it's positive.
+all_numbers = [111, 32, -9, -45, -17, 9, 85, -10]
+
+# --- WRITE YOUR CODE BELOW ---
+
+
+
+
+
+
+
+# Exercise 5:
+# Write a for loop to print the following pattern:
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5
+
+# --- WRITE YOUR CODE BELOW ---
+
+
+
+
+
+
+
+
+
+# Exercise 6:
+# Calculate the sum of all numbers from 1 to a given number 
+# The number given should be given by the user - hint: use input()
+
+# --- WRITE YOUR CODE BELOW ---
+
+
+
+
+
+
+
+
+
+
+
+# Exercise 7: 
+# Write a program to print multiplication table of a given number
+# (again via user input)
+
+# for example num = 2
+# the output should be:
+# 2
+# 4
+# 6
+# 8
+# 10
+# 12
+# 14
+# 16
+# 18
+# 20
+
+# --- WRITE YOUR CODE BELOW ---
+
+
+
+
+
+
+
+# Exercise 8: Count the total number of digits in a number
+# Write a program to count the total number of digits in a number using a while loop.
+
+# For example, the number is 75869, so the output should be 5.
+
+# --- WRITE YOUR CODE BELOW ---
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Exercise 9: Print list in reverse order using a loop
+# Print the items of the given list in reverse
+reverse_this_list = [10, 20, 30, 40, 50]
+
+# --- WRITE YOUR CODE BELOW ---
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Exercise 10: Find the factorial of a given number
+
+# Write a program to use the loop to find the factorial of a given number. The number should be given by user input.
+# The factorial (symbol: !) means to multiply all whole numbers from the chosen number down to 1.
+# For example: calculate the factorial of 5: 
+# 5! = 5 × 4 × 3 × 2 × 1 = 120
+# Expected output for 5: 120
+
+# --- WRITE YOUR CODE BELOW ---
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Exercise 11: Reverse a given integer number. The number should be given by user input
+# Example: 76542 should output 24567
+
+# --- WRITE YOUR CODE BELOW ---
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
