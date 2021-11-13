1a3548f8c572e32334cabd368ac2536725ffe859
Máté Bendegúz Kovács <20370225+kmb5@users.noreply.github.com>
2021-11-13 08:41:19 +0100

    Update loop_exercises.py

--- loop_exercises.py
+++ loop_exercises.py
@@ -46,25 +46,9 @@ all_numbers = [111, 32, -9, -45, -17, 9, 85, -10]
 
 
 
+
+
 # Exercise 5:
-# Write a for loop to print the following pattern:
-# 1 
-# 1 2 
-# 1 2 3 
-# 1 2 3 4 
-# 1 2 3 4 5
-
-# --- WRITE YOUR CODE BELOW ---
-
-
-
-
-
-
-
-
-
-# Exercise 6:
 # Calculate the sum of all numbers from 1 to a given number 
 # The number given should be given by the user - hint: use input()
 
@@ -80,7 +64,7 @@ all_numbers = [111, 32, -9, -45, -17, 9, 85, -10]
 
 
 
-# Exercise 7: 
+# Exercise 6: 
 # Write a program to print multiplication table of a given number
 # (again via user input)
 
@@ -105,7 +89,7 @@ all_numbers = [111, 32, -9, -45, -17, 9, 85, -10]
 
 
 
-# Exercise 8: Count the total number of digits in a number
+# Exercise 7: Count the total number of digits in a number
 # Write a program to count the total number of digits in a number using a while loop.
 
 # For example, the number is 75869, so the output should be 5.
@@ -125,7 +109,7 @@ all_numbers = [111, 32, -9, -45, -17, 9, 85, -10]
 
 
 
-# Exercise 9: Print list in reverse order using a loop
+# Exercise 8: Print list in reverse order using a loop
 # Print the items of the given list in reverse
 reverse_this_list = [10, 20, 30, 40, 50]
 
@@ -146,7 +130,7 @@ reverse_this_list = [10, 20, 30, 40, 50]
 
 
 
-# Exercise 10: Find the factorial of a given number
+# Exercise 9: Find the factorial of a given number
 
 # Write a program to use the loop to find the factorial of a given number. The number should be given by user input.
 # The factorial (symbol: !) means to multiply all whole numbers from the chosen number down to 1.
@@ -172,7 +156,7 @@ reverse_this_list = [10, 20, 30, 40, 50]
 
 
 
-# Exercise 11: Reverse a given integer number. The number should be given by user input
+# Exercise 10: Reverse a given integer number. The number should be given by user input
 # Example: 76542 should output 24567
 
 # --- WRITE YOUR CODE BELOW ---
